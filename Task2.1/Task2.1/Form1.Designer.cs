7fe9f1007c2a71111fd60cf970d63008b034d15f
pem-pem18 <64434739+pem-pem18@users.noreply.github.com>
2021-10-09 18:54:09 +0300

    Add files via upload

---
+++ Task2.1/Task2.1/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿
+namespace Task2._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Client = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PhoneNumber = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Email = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.IDBox = new System.Windows.Forms.TextBox();
+            this.ClientBox = new System.Windows.Forms.TextBox();
+            this.PhoneNumberBox = new System.Windows.Forms.TextBox();
+            this.EmailBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.ImportButton = new System.Windows.Forms.Button();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.AddButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.Client,
+            this.PhoneNumber,
+            this.Email});
+            this.dataGridView.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.ReadOnly = true;
+            this.dataGridView.RowHeadersWidth = 51;
+            this.dataGridView.RowTemplate.Height = 24;
+            this.dataGridView.Size = new System.Drawing.Size(476, 426);
+            this.dataGridView.TabIndex = 0;
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "ID";
+            this.ID.MinimumWidth = 6;
+            this.ID.Name = "ID";
+            this.ID.ReadOnly = true;
+            this.ID.Width = 125;
+            // 
+            // Client
+            // 
+            this.Client.HeaderText = "Client";
+            this.Client.MinimumWidth = 6;
+            this.Client.Name = "Client";
+            this.Client.ReadOnly = true;
+            this.Client.Width = 125;
+            // 
+            // PhoneNumber
+            // 
+            this.PhoneNumber.HeaderText = "PhoneNumber";
+            this.PhoneNumber.MinimumWidth = 6;
+            this.PhoneNumber.Name = "PhoneNumber";
+            this.PhoneNumber.ReadOnly = true;
+            this.PhoneNumber.Width = 125;
+            // 
+            // Email
+            // 
+            this.Email.HeaderText = "Email";
+            this.Email.MinimumWidth = 6;
+            this.Email.Name = "Email";
+            this.Email.ReadOnly = true;
+            this.Email.Width = 125;
+            // 
+            // IDBox
+            // 
+            this.IDBox.Location = new System.Drawing.Point(494, 12);
+            this.IDBox.Name = "IDBox";
+            this.IDBox.Size = new System.Drawing.Size(200, 22);
+            this.IDBox.TabIndex = 1;
+            // 
+            // ClientBox
+            // 
+            this.ClientBox.Location = new System.Drawing.Point(495, 41);
+            this.ClientBox.Name = "ClientBox";
+            this.ClientBox.Size = new System.Drawing.Size(199, 22);
+            this.ClientBox.TabIndex = 2;
+            // 
+            // PhoneNumberBox
+            // 
+            this.PhoneNumberBox.Location = new System.Drawing.Point(495, 70);
+            this.PhoneNumberBox.Name = "PhoneNumberBox";
+            this.PhoneNumberBox.Size = new System.Drawing.Size(199, 22);
+            this.PhoneNumberBox.TabIndex = 3;
+            // 
+            // EmailBox
+            // 
+            this.EmailBox.Location = new System.Drawing.Point(495, 99);
+            this.EmailBox.Name = "EmailBox";
+            this.EmailBox.Size = new System.Drawing.Size(199, 22);
+            this.EmailBox.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(700, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(700, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Client";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(700, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "PhoneNumber";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(700, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Email";
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Location = new System.Drawing.Point(495, 357);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(198, 23);
+            this.ClearButton.TabIndex = 9;
+            this.ClearButton.Text = "Очистить таблицу";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // ImportButton
+            // 
+            this.ImportButton.Location = new System.Drawing.Point(496, 386);
+            this.ImportButton.Name = "ImportButton";
+            this.ImportButton.Size = new System.Drawing.Size(198, 23);
+            this.ImportButton.TabIndex = 10;
+            this.ImportButton.Text = "Импортировать из файла";
+            this.ImportButton.UseVisualStyleBackColor = true;
+            this.ImportButton.Click += new System.EventHandler(this.ImportButton_Click);
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(496, 415);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(198, 23);
+            this.SaveButton.TabIndex = 11;
+            this.SaveButton.Text = "Сохранить";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // AddButton
+            // 
+            this.AddButton.Location = new System.Drawing.Point(496, 127);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(198, 23);
+            this.AddButton.TabIndex = 12;
+            this.AddButton.Text = "Добавить";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.AddButton);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.ImportButton);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.EmailBox);
+            this.Controls.Add(this.PhoneNumberBox);
+            this.Controls.Add(this.ClientBox);
+            this.Controls.Add(this.IDBox);
+            this.Controls.Add(this.dataGridView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.TextBox IDBox;
+        private System.Windows.Forms.TextBox ClientBox;
+        private System.Windows.Forms.TextBox PhoneNumberBox;
+        private System.Windows.Forms.TextBox EmailBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.Button ImportButton;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Client;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PhoneNumber;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Email;
+    }
+}
+
---
+++ Task2.1/Task2.1/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Task2._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        // клик по кнопке <добавить значения боксов в соответствующие столбцы datagridview>
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            if (IDBox.Text == "" || ClientBox.Text == "" || PhoneNumberBox.Text == "" || EmailBox.Text == "")
+            {
+                MessageBox.Show("Недопустимое значение поля.", "Ошибка"); // проверка на пустоту боксов
+            }
+            else
+            {
+                int n = 0;
+
+                //if (dataGridView.Rows.Count == 0)
+                //    n = dataGridView.Rows.Add(); // взятие индекса каждой новой строки
+                //else
+                //{
+                //    n = dataGridView.Rows.Count;
+
+                //    //// загрузка файла в объект
+                //    //XDocument xDoc = XDocument.Load("Test.xml");
+                //    //foreach (XElement root in xDoc.Descendants("NewDataSet"))
+                //    //    n = root.Elements("Contract").Count();
+                //}
+
+                n = dataGridView.Rows.Add(); // взятие индекса каждой новой строки
+
+                dataGridView.Rows[n].Cells[0].Value = IDBox.Text;
+                dataGridView.Rows[n].Cells[1].Value = ClientBox.Text;
+                dataGridView.Rows[n].Cells[2].Value = PhoneNumberBox.Text;
+                dataGridView.Rows[n].Cells[3].Value = EmailBox.Text;
+
+                // dataGridView.Rows.Add(IDBox.Text, ClientBox.Text, PhoneNumberBox.Text, EmailBox.Text);
+
+                // очистка боксов для последующего ввода
+                IDBox.Clear();
+                ClientBox.Clear();
+                PhoneNumberBox.Clear();
+                EmailBox.Clear();
+            }
+        }
+
+        // клик по кнопке <очиститть строки datagridview>
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            dataGridView.Rows.Clear();
+        }
+
+        // клик по кнопке <импорт из файла в datagridview>
+        private void ImportButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // загрузка файла в объект
+                XDocument xDoc = XDocument.Load("Test.xml");
+
+                // обход всех узлов в корневом элементе
+                foreach (XElement root in xDoc.Descendants("NewDataSet"))
+                {
+                    if (root.HasElements)
+                    {
+                        dataGridView.Rows.Clear();
+
+                        // обходим все дочерние узлы элемента Contract
+                        foreach (XElement elm in xDoc.Descendants("Contract"))
+                        {
+                            // чтение элементов
+                            if (elm.Element("ID") != null && elm.Element("Client") != null && elm.Element("PhoneNumber") != null && elm.Element("Email") != null)
+                                dataGridView.Rows.Add(elm.Element("ID").Value, elm.Element("Client").Value, elm.Element("PhoneNumber").Value, elm.Element("Email").Value);
+                        }
+                    }
+                }
+
+                ////создаём таблицу
+                //DataTable dt = new DataTable("NewDataSet");
+
+                ////создаём 4 колонки
+                //DataColumn ID = new DataColumn("ID", typeof(String));
+                //DataColumn Client = new DataColumn("Client", typeof(String));
+                //DataColumn PhoneNumber = new DataColumn("PhoneNumber", typeof(String));
+                //DataColumn Email = new DataColumn("Email", typeof(String));
+
+                ////добавляем колонки в таблицу
+                //dt.Columns.Add(ID);
+                //dt.Columns.Add(Client);
+                //dt.Columns.Add(PhoneNumber);
+                //dt.Columns.Add(Email);
+
+                //DataRow newRow = null;
+
+                //// загрузка файла в объект
+                //XDocument xDoc = XDocument.Load("Test.xml");
+
+                //// обход всех узлов в корневом элементе
+                //foreach (XElement root in xDoc.Descendants("NewDataSet"))
+                //{
+                //    if (root.HasElements)
+                //    {
+                //        // обходим все дочерние узлы элемента Contract
+                //        foreach (XElement elm in xDoc.Descendants("Contract"))
+                //        {
+                //            newRow = dt.NewRow();
+
+                //            // чтение каждого элемента
+                //            if (elm.Element("ID") != null)
+                //                newRow["ID"] = int.Parse(elm.Element("ID").Value);
+                //            if (elm.Element("Client") != null)
+                //                newRow["Client"] = int.Parse(elm.Element("Client").Value);
+                //            if (elm.Element("PhoneNumber") != null)
+                //                newRow["PhoneNumber"] = int.Parse(elm.Element("PhoneNumber").Value);
+                //            if (elm.Element("Email") != null)
+                //                newRow["Email"] = int.Parse(elm.Element("Email").Value);
+
+                //            dataGridView.Columns.Clear(); // очистка столбцов datagridview
+
+                //            dt.Rows.Add(newRow); //добавляем новую запись в таблицу
+                //        }
+                //    }
+                //}
+                //dataGridView.DataSource = dt; // добавляем в datagrid
+
+                // // отвязываем данные от dgv
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка"); // вывод ошибки
+            }
+        }
+
+        // клик по кнопке <сохранить данные в XML>
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataSet ds = new DataSet(); // создаем пустой кэш данных
+                DataTable dt = new DataTable(); // создаем пустую таблицу данных
+
+                dt.TableName = "Contract";
+                dt.Columns.Add("ID");
+                dt.Columns.Add("Client");
+                dt.Columns.Add("PhoneNumber");
+                dt.Columns.Add("Email");
+
+                ds.Tables.Add(dt);
+                
+                foreach (DataGridViewRow r in dataGridView.Rows) // пока в datagridview есть строки
+                {
+                    DataRow row = ds.Tables["Contract"].NewRow(); // создаем новую строку в таблице, занесенной в ds
+
+                    // в столбец каждой из строк заносим значение соответствующего стоблца datagridview
+                    row["ID"] = r.Cells[0].Value;
+                    row["Client"] = r.Cells[1].Value;
+                    row["PhoneNumber"] = r.Cells[2].Value;
+                    row["Email"] = r.Cells[3].Value;
+
+                    ds.Tables["Contract"].Rows.Add(row);
+                }
+                ds.WriteXml("Test.xml");
+                MessageBox.Show("XML файл успешно сохранен.", "Выполнено");
+            }
+            catch
+            {
+                MessageBox.Show("Невозможно сохранить XML файл.", "Ошибка");
+            }
+        }
+    }
+}
